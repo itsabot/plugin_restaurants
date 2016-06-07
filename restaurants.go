6fda1ebf288191aa550a570416416314051d5000
Evan Tann <egtann@gmail.com>
2016-06-06 21:40:44 -0700

    Add basic functionality

--- restaurants.go
+++ restaurants.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net/url"
 	"os"
+	"strings"
 
 	"github.com/garyburd/go-oauth/oauth"
 	"github.com/itsabot/abot/core/log"
@@ -21,22 +22,22 @@ type client struct {
 	token  oauth.Credentials
 }
 
-type yelpResp struct {
-	Businesses []struct {
-		Name         string
-		ImageURL     string `json:"image_url"`
-		MobileURL    string `json:"mobile_url"`
-		DisplayPhone string `json:"display_phone"`
-		Distance     int
-		Rating       float64
-		Location     struct {
-			City           string
-			DisplayAddress []string `json:"display_address"`
-		}
+type business struct {
+	Name         string
+	ImageURL     string `json:"image_url"`
+	MobileURL    string `json:"mobile_url"`
+	DisplayPhone string `json:"display_phone"`
+	Distance     int
+	Rating       float64
+	Location     struct {
+		City           string
+		DisplayAddress []string `json:"display_address"`
 	}
 }
 
-var ErrNoBusinesses = errors.New("no businesses")
+type yelpResp struct {
+	Businesses []business
+}
 
 var c client
 var p *dt.Plugin
@@ -47,7 +48,12 @@ func init() {
 	c.client.Credentials.Secret = os.Getenv("YELP_CONSUMER_SECRET")
 	c.token.Token = os.Getenv("YELP_TOKEN")
 	c.token.Secret = os.Getenv("YELP_TOKEN_SECRET")
-	for _, food := range language.Foods() {
+	foods := language.Join(
+		language.Foods(),
+		language.Restaurants(),
+		language.Desserts(),
+	)
+	for _, food := range foods {
 		foodMap[food] = struct{}{}
 	}
 	var err error
@@ -57,7 +63,7 @@ func init() {
 	}
 	plugin.SetKeywords(p,
 		dt.KeywordHandler{
-			Fn: kwGetRestaurant,
+			Fn: kwRecommendRestaurants,
 			Trigger: &dt.StructuredInput{
 				Commands: []string{
 					"find",
@@ -67,7 +73,84 @@ func init() {
 					"recommend",
 					"recommendation",
 				},
-				Objects: language.Foods(),
+				Objects: foods,
+			},
+		},
+		dt.KeywordHandler{
+			Fn: kwGetPhone,
+			Trigger: &dt.StructuredInput{
+				Commands: []string{
+					"find",
+					"what",
+					"show",
+					"call",
+					"have",
+				},
+				Objects: []string{"phone", "number"},
+			},
+		},
+		dt.KeywordHandler{
+			Fn: kwGetAddress,
+			Trigger: &dt.StructuredInput{
+				Commands: []string{
+					"find",
+					"what",
+					"show",
+					"navigate",
+					"where",
+					"have",
+				},
+				Objects: []string{"address", "directions", "located"},
+			},
+		},
+		dt.KeywordHandler{
+			Fn: kwGetRating,
+			Trigger: &dt.StructuredInput{
+				Commands: []string{
+					"find",
+					"what",
+					"show",
+					"have",
+				},
+				Objects: []string{"rating", "rated", "review"},
+			},
+		},
+		dt.KeywordHandler{
+			Fn: kwGetPictures,
+			Trigger: &dt.StructuredInput{
+				Commands: []string{
+					"find",
+					"show",
+					"have",
+					"see",
+				},
+				Objects: []string{"pictures", "pics"},
+			},
+		},
+		dt.KeywordHandler{
+			Fn: kwGetMenu,
+			Trigger: &dt.StructuredInput{
+				Commands: []string{
+					"find",
+					"what",
+					"show",
+					"have",
+					"see",
+				},
+				Objects: []string{"menu"},
+			},
+		},
+		dt.KeywordHandler{
+			Fn: kwIterate,
+			Trigger: &dt.StructuredInput{
+				Commands: []string{
+					"find",
+					"what",
+					"show",
+					"have",
+					"see",
+				},
+				Objects: []string{"something", "else", "different"},
 			},
 		},
 	)
@@ -78,7 +161,7 @@ func init() {
 					return "Where are you now?"
 				},
 				OnInput: func(in *dt.Msg) {
-					p.SetMemory(in, prefs.Location, in.Sentence)
+					extractAndSaveCities(in)
 				},
 				Complete: func(in *dt.Msg) (bool, string) {
 					return p.HasMemory(in, prefs.Location), ""
@@ -90,85 +173,86 @@ func init() {
 					return "Ok. What kind of restaurant are you looking for?"
 				},
 				OnInput: func(in *dt.Msg) {
-					restaurants := recommendRestaurants(in)
-					p.SetMemory(in, "restaurantSearchResults",
-						restaurants)
-					p.SetMemory(in, "restaurantSearchResultsStrings",
-						restaurants)
+					_ = kwRecommendRestaurants(in)
 				},
 				Complete: func(in *dt.Msg) (bool, string) {
-					return p.HasMemory(in, "restaurantSearchResults"), ""
+					var complete bool
+					if p.HasMemory(in, "restaurantType") {
+						complete = true
+					}
+					return complete, ""
 				},
+				SkipIfComplete: true,
 			},
 		},
-		task.Iterate(p, "", task.OptsIterate{
-			IterableMemKey: "restaurantSearchResults",
-			ResultMemKey:   "selectedRestaurant",
+		task.Iterate(p, "iterate", task.OptsIterate{
+			IterableMemKey:  "restaurantSearchResultsStrings",
+			ResultMemKeyIdx: "selectedRestaurantIdx",
 		}),
 		{
 			{
 				OnEntry: func(in *dt.Msg) string {
-					mem := p.GetMemory(in, "selectedRestaurant")
-					return "You selected " + mem.String()
+					return "Great! Let me know if you'd like to get the address or phone number, or see reviews, pictures, or the menu."
 				},
-				OnInput: func(in *dt.Msg) {
-
-				},
+				OnInput: func(in *dt.Msg) {},
 				Complete: func(in *dt.Msg) (bool, string) {
-					return true, ""
+					// Notice that we keep the user in this
+					// state. This allows the user to
+					// continue asking questions without
+					// memory being reset. We do this
+					// because when the user selects a
+					// restaurant, they usually aren't
+					// "done," since they'll now need the
+					// phone number, etc.
+					return false, ""
 				},
 			},
 		},
 	})
 	p.SM.SetOnReset(func(in *dt.Msg) {
-		p.DeleteMemory(in, "restaurantSearchResults")
-		p.DeleteMemory(in, "selectedRestaurant")
 		task.ResetIterate(p, in)
+		p.DeleteMemory(in, "restaurantSearchResults")
+		p.DeleteMemory(in, "restaurantSearchResultsStrings")
+		p.DeleteMemory(in, "selectedRestaurantIdx")
+		p.DeleteMemory(in, "restaurantType")
 	})
 	if err = plugin.Register(p); err != nil {
 		p.Log.Fatal("failed to register restaurants plugin.", err)
 	}
 }
 
-func kwGetRestaurant(in *dt.Msg) string {
+func kwRecommendRestaurants(in *dt.Msg) string {
 	var foods string
 	for _, t := range in.Tokens {
-		_, ok := foodMap[t]
+		_, ok := foodMap[strings.ToLower(t)]
 		if ok {
 			foods += t + " "
 			continue
 		}
 	}
-	cities, _ := language.ExtractCities(p.DB, in)
-	if len(cities) > 0 {
-		p.SetMemory(in, prefs.Location, cities[0].Name)
+	p.SetMemory(in, "restaurantType", foods)
+	extractAndSaveCities(in)
+	if len(foods) == 0 || !p.HasMemory(in, prefs.Location) {
+		return ""
 	}
-	return ""
-}
-
-func recommendRestaurants(in *dt.Msg) []string {
 	form := url.Values{
-		"term":     {in.Sentence + " restaurant"},
+		"term":     {foods + " restaurant"},
 		"location": {p.GetMemory(in, prefs.Location).String()},
-		"limit":    {fmt.Sprintf("%.0f", 10)},
+		"limit":    {fmt.Sprintf("%.0f", 10.0)},
 	}
 	var data yelpResp
 	err := c.get("http://api.yelp.com/v2/search", form, &data)
 	if err != nil {
-		/*
-			m.Sentence = "I can't find that for you now. " +
-				"Let's try again later."
-			return err
-		*/
-		// return for confused response, given Yelp errors are rare, but
-		// unintentional runs of Yelp queries are much more common
-		return nil
+		log.Info("failed to make yelp query.", err)
+		return ""
 	}
 	var names []string
 	for _, biz := range data.Businesses {
 		names = append(names, biz.Name)
 	}
-	return names
+	p.SetMemory(in, "restaurantSearchResults", data.Businesses)
+	p.SetMemory(in, "restaurantSearchResultsStrings", names)
+	return ""
 }
 
 func (c *client) get(urlStr string, params url.Values, v interface{}) error {
@@ -183,90 +267,81 @@ func (c *client) get(urlStr string, params url.Values, v interface{}) error {
 	return json.NewDecoder(resp.Body).Decode(v)
 }
 
-/*
-	// Responses were returned, and the user has asked this plugin an
-	// additional query. Handle the query by keyword
-	words := strings.Fields(*resp)
-	offI := int(m.State["offset"].(float64))
-	var s string
-	for _, w := range words {
-		w = strings.TrimRight(w, ").,;?!:")
-		switch strings.ToLower(w) {
-		case "rated", "rating", "review", "recommend", "recommended":
-			s = fmt.Sprintf("It has a %s star review on Yelp",
-				getRating(m, offI))
-			*resp = s
-		case "number", "phone":
-			s = getPhone(m, offI)
-			*resp = s
-		case "call":
-			s = fmt.Sprintf("You can reach them here: %s",
-				getPhone(m, offI))
-			*resp = s
-		case "information", "info":
-			s = fmt.Sprintf("Here's some more info: %s",
-				getURL(m, offI))
-			*resp = s
-		case "where", "location", "address", "direction", "directions",
-			"addr":
-			s = fmt.Sprintf("It's at %s", getAddress(m, offI))
-			*resp = s
-		case "pictures", "pic", "pics":
-			s = fmt.Sprintf("I found some pics here: %s",
-				getURL(m, offI))
-			*resp = s
-		case "menu", "have":
-			s = fmt.Sprintf("Yelp might have a menu... %s",
-				getURL(m, offI))
-			*resp = s
-		case "not", "else", "no", "anything", "something":
-			m.State["offset"] = float64(offI + 1)
-			if err := t.searchYelp(m, resp); err != nil {
-				return err
-			}
-		// TODO perhaps handle this case and "thanks" at the Abot level?
-		// with bayesian classification
-		case "good", "great", "yes", "perfect":
-			// TODO feed into learning engine
-			*resp = language.Positive()
-		case "thanks", "thank":
-			*resp = language.Welcome()
-		}
-		if len(*resp) > 0 {
-			return nil
-		}
+func extractAndSaveCities(in *dt.Msg) {
+	cities, _ := language.ExtractCities(p.DB, in)
+	if len(cities) > 0 {
+		p.SetMemory(in, prefs.Location, cities[0].Name)
 	}
-	/*
-
-func getRating(r *dt.Msg, offset int) string {
-	businesses := r.State["businesses"].([]interface{})
-	firstBusiness := businesses[offset].(map[string]interface{})
-	return fmt.Sprintf("%.1f", firstBusiness["Rating"].(float64))
 }
 
-func getURL(r *dt.Msg, offset int) string {
-	businesses := r.State["businesses"].([]interface{})
-	firstBusiness := businesses[offset].(map[string]interface{})
-	return firstBusiness["mobile_url"].(string)
-}
-
-func getPhone(r *dt.Msg, offset int) string {
-	businesses := r.State["businesses"].([]interface{})
-	firstBusiness := businesses[offset].(map[string]interface{})
-	return firstBusiness["display_phone"].(string)
-}
-
-func getAddress(r *dt.Msg, offset int) string {
-	businesses := r.State["businesses"].([]interface{})
-	firstBusiness := businesses[offset].(map[string]interface{})
-	location := firstBusiness["Location"].(map[string]interface{})
-	dispAddr := location["display_address"].([]interface{})
-	if len(dispAddr) > 1 {
-		str1 := dispAddr[0].(string)
-		str2 := dispAddr[1].(string)
-		return fmt.Sprintf("%s in %s", str1, str2)
+func kwGetPhone(in *dt.Msg) string {
+	biz, err := getBusiness(in)
+	if err != nil {
+		return ""
 	}
-	return dispAddr[0].(string)
+	return "The number is " + biz.DisplayPhone
 }
 
-*/
+func kwGetAddress(in *dt.Msg) string {
+	biz, err := getBusiness(in)
+	if err != nil {
+		return ""
+	}
+	addr := strings.Join(biz.Location.DisplayAddress, "\n")
+	return "The restaurant is at:\n\n" + addr
+}
+
+func kwGetPictures(in *dt.Msg) string {
+	biz, err := getBusiness(in)
+	if err != nil {
+		return ""
+	}
+	return "Their Yelp page probably has some great pictures: " + biz.ImageURL
+}
+
+func kwGetRating(in *dt.Msg) string {
+	biz, err := getBusiness(in)
+	if err != nil {
+		return ""
+	}
+	f := fmt.Sprintf("%.1f", biz.Rating)
+	return "They have a " + f + " star rating on Yelp."
+}
+
+func kwGetMenu(in *dt.Msg) string {
+	biz, err := getBusiness(in)
+	if err != nil {
+		return ""
+	}
+	return "Yelp might have a menu for them: " + biz.MobileURL
+}
+
+func kwIterate(in *dt.Msg) string {
+	return p.SM.SetState(in, "iterate")
+}
+
+func getBusinesses(in *dt.Msg) []business {
+	var businesses []business
+	mem := p.GetMemory(in, "restaurantSearchResults")
+	err := json.Unmarshal(mem.Val, &businesses)
+	if err != nil {
+		log.Info("failed to unmarshal businesses.", err)
+		return nil
+	}
+	return businesses
+}
+
+func getBusiness(in *dt.Msg) (*business, error) {
+	idx := int64(-1)
+	if p.HasMemory(in, "selectedRestaurantIdx") {
+		idx = p.GetMemory(in, "selectedRestaurantIdx").Int64()
+	}
+	if idx < 0 {
+		return nil, errors.New("missing business")
+	}
+	businesses := getBusinesses(in)
+	if businesses == nil {
+		return nil, errors.New("missing businesses")
+	}
+	return &businesses[idx], nil
+}
--- restaurants_test.go
+++ restaurants_test.go
@@ -10,14 +10,14 @@ import (
 	"testing"
 
 	"github.com/itsabot/abot/core"
-	"github.com/itsabot/abot/shared/datatypes"
+	"github.com/itsabot/abot/core/log"
 	"github.com/julienschmidt/httprouter"
-	"github.com/labstack/gommon/log"
 )
 
 var r *httprouter.Router
 
 func TestMain(m *testing.M) {
+	cleanup()
 	err := os.Setenv("ABOT_ENV", "test")
 	if err != nil {
 		log.Fatal("failed to set ABOT_ENV.", err)
@@ -27,57 +27,39 @@ func TestMain(m *testing.M) {
 		log.Fatal("failed to start abot server.", err)
 	}
 	exitVal := m.Run()
-	q := `DELETE FROM messages`
-	_, err = p.DB.Exec(q)
-	if err != nil {
-		log.Info("failed to delete messages.", err)
-	}
-	q = `DELETE FROM states`
-	_, err = p.DB.Exec(q)
-	if err != nil {
-		log.Info("failed to delete messages.", err)
-	}
+	cleanup()
 	os.Exit(exitVal)
 }
 
-func TestKWGetRestaurant(t *testing.T) {
+func TestKWRecommendRestaurants(t *testing.T) {
 	// Map test sentences with the important content that must be contained
 	// in the reply.
-	tests := map[string]string{
-		"Find me a Thai restaurant in SF.": "How about",
-		"Where's ramen in LA?":             "How about",
-		"Where's a ramen restaurant?":      "Where are you now?",
-		"Find me a restaurant":             "Where are you now?",
+	seqTests := []string{
+		"Find me a restaurant",
+		"Where are you now?",
+		"I'm in LA",
+		"What kind of restaurant",
+		"Thai",
+		"How about",
+		"No",
+		"How about",
+		"Yes",
+		"Great! Let me know",
+		"What's the address?",
+		"The restaurant is at",
+		"What's the phone number?",
+		"The number is",
+		"Show me some pics",
+		"Their Yelp page probably has",
+		"What are they rated?",
+		"star rating on Yelp",
+		"Can I see a menu?",
+		"Yelp might have a menu for them",
+		"Show me something else",
+		"How about",
 	}
-	for test, expected := range tests {
-		data := struct {
-			FlexIDType int
-			FlexID     string
-			CMD        string
-		}{
-			FlexIDType: 3,
-			FlexID:     "0",
-			CMD:        test,
-		}
-		byt, err := json.Marshal(data)
-		if err != nil {
-			t.Fatal("failed to marshal req.", err)
-		}
-		c, b := request("POST", os.Getenv("ABOT_URL")+"/", byt)
-		if c != http.StatusOK {
-			t.Fatal("expected", http.StatusOK, "got", c, b)
-		}
-		if !strings.Contains(b, expected) {
-			t.Fatalf("expected %q, got %q\n", expected, b)
-		}
-		in := core.NewMsg(&dt.User{ID: 1}, test)
-		res := kwGetRestaurant(in)
-		if len(expected) == 0 && len(res) > 0 {
-			t.Fatalf("expected %q, got %q\n", expected, res)
-		}
-		if !strings.Contains(res, expected) {
-			t.Fatalf("expected %q, got %q\n", expected, res)
-		}
+	for i := 0; i+1 < len(seqTests); i += 2 {
+		testReq(t, seqTests[i], seqTests[i+1])
 	}
 }
 
@@ -89,4 +71,40 @@ func request(method, path string, data []byte) (int, string) {
 	w := httptest.NewRecorder()
 	r.ServeHTTP(w, req)
 	return w.Code, string(w.Body.Bytes())
+}
+
+func testReq(t *testing.T, in, exp string) {
+	data := struct {
+		FlexIDType int
+		FlexID     string
+		CMD        string
+	}{
+		FlexIDType: 3,
+		FlexID:     "0",
+		CMD:        in,
+	}
+	byt, err := json.Marshal(data)
+	if err != nil {
+		t.Fatal("failed to marshal req.", err)
+	}
+	c, b := request("POST", os.Getenv("ABOT_URL")+"/", byt)
+	if c != http.StatusOK {
+		t.Fatal("exp", http.StatusOK, "got", c, b)
+	}
+	if !strings.Contains(b, exp) {
+		t.Fatalf("exp %q, got %q for %q\n", exp, b, in)
+	}
+}
+
+func cleanup() {
+	q := `DELETE FROM messages`
+	_, err := p.DB.Exec(q)
+	if err != nil {
+		log.Info("failed to delete messages.", err)
+	}
+	q = `DELETE FROM states`
+	_, err = p.DB.Exec(q)
+	if err != nil {
+		log.Info("failed to delete messages.", err)
+	}
 }
