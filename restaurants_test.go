cf65e69238948717c6bafab6033a656a753240f8
Evan Tann <egtann@gmail.com>
2016-06-09 14:21:23 -0700

    Update plugin test API

--- restaurants_test.go
+++ restaurants_test.go
@@ -1,34 +1,18 @@
 package restaurants
 
 import (
-	"bytes"
-	"encoding/json"
-	"net/http"
-	"net/http/httptest"
 	"os"
-	"strings"
 	"testing"
 
-	"github.com/itsabot/abot/core"
-	"github.com/itsabot/abot/core/log"
+	"github.com/itsabot/abot/shared/plugin"
 	"github.com/julienschmidt/httprouter"
 )
 
 var r *httprouter.Router
 
 func TestMain(m *testing.M) {
-	cleanup()
-	err := os.Setenv("ABOT_ENV", "test")
-	if err != nil {
-		log.Fatal("failed to set ABOT_ENV.", err)
-	}
-	r, err = core.NewServer()
-	if err != nil {
-		log.Fatal("failed to start abot server.", err)
-	}
-	exitVal := m.Run()
-	cleanup()
-	os.Exit(exitVal)
+	r = plugin.TestPrepare()
+	os.Exit(m.Run())
 }
 
 func TestKWRecommendRestaurants(t *testing.T) {
@@ -59,52 +43,9 @@ func TestKWRecommendRestaurants(t *testing.T) {
 		"How about",
 	}
 	for i := 0; i+1 < len(seqTests); i += 2 {
-		testReq(t, seqTests[i], seqTests[i+1])
-	}
-}
-
-func request(method, path string, data []byte) (int, string) {
-	req, err := http.NewRequest(method, path, bytes.NewBuffer(data))
-	if err != nil {
-		return 0, "err completing request: " + err.Error()
-	}
-	w := httptest.NewRecorder()
-	r.ServeHTTP(w, req)
-	return w.Code, string(w.Body.Bytes())
-}
-
-func testReq(t *testing.T, in, exp string) {
-	data := struct {
-		FlexIDType int
-		FlexID     string
-		CMD        string
-	}{
-		FlexIDType: 3,
-		FlexID:     "0",
-		CMD:        in,
-	}
-	byt, err := json.Marshal(data)
-	if err != nil {
-		t.Fatal("failed to marshal req.", err)
-	}
-	c, b := request("POST", os.Getenv("ABOT_URL")+"/", byt)
-	if c != http.StatusOK {
-		t.Fatal("exp", http.StatusOK, "got", c, b)
-	}
-	if !strings.Contains(b, exp) {
-		t.Fatalf("exp %q, got %q for %q\n", exp, b, in)
-	}
-}
-
-func cleanup() {
-	q := `DELETE FROM messages`
-	_, err := p.DB.Exec(q)
-	if err != nil {
-		log.Info("failed to delete messages.", err)
-	}
-	q = `DELETE FROM states`
-	_, err = p.DB.Exec(q)
-	if err != nil {
-		log.Info("failed to delete messages.", err)
+		err := plugin.TestReq(r, seqTests[i], seqTests[i+1])
+		if err != nil {
+			t.Fatal(err)
+		}
 	}
 }
